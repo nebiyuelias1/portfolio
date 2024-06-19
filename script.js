fe564fe09af2a75204efb1ab06c4918aa7f8bb76
nebiyuelias1 <nebiyuelias1@gmail.com>
2024-06-19 15:17:09 +0800

    feat: add mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const scrollButton = document.querySelector("#scrollButton");
 const main = document.querySelector("#main");
+const navLink = document.querySelector('.nav-link');
 
 scrollButton.addEventListener("click", () => {
   // 64 (magic number) is the height of the navbar. 
@@ -11,3 +12,15 @@ scrollButton.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+function toggleMenu() {
+  const menuIcon = document.querySelector("#menu-icon");
+
+  navLink.classList.toggle("active");
+  menuIcon.classList.toggle("active"); // Toggle the active class on the menu icon
+}
+
+// This will allow to close the menu when a link is clicked
+// for smaller screens. Otherwise, the menu will remain open
+// after a link is clicked.
+navLink.addEventListener('click', toggleMenu);
